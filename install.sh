89be9871226a206a2e9dcea2a1b57fca475d29e6
Sharad Vikram <sharad.vikram@gmail.com>
2016-12-02 20:18:55 -0800

    updated scripts

--- install.sh
+++ install.sh
@@ -6,16 +6,15 @@ EXTRA_DIR="$HOME/.extra"
 
 [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
-echo "source ~/.bashrc.local" >> ~/.bashrc
-
 curl http://j.mp/spf13-vim3 -L -o - | sh
 
 ln -sfv "$DOTFILES_DIR/bash/bashrc" ~/.bashrc.local
 ln -sfv "$DOTFILES_DIR/bash/bash_aliases" ~/.bash_aliases
 ln -sfv "$DOTFILES_DIR/bash/bash_functions" ~/.bash_functions
+ln -sfv "$DOTFILES_DIR/bash/bash_git" ~/.bash_git
 
 ln -sfv "$DOTFILES_DIR/vim/vimrc.local" ~/.vimrc.local
-ln -sfv "$DOTFILES_DIR/bash/vimrc.bundles.local" ~/.vimrc.bundles.local
+ln -sfv "$DOTFILES_DIR/vim/vimrc.bundles.local" ~/.vimrc.bundles.local
 
 ln -sfv "$DOTFILES_DIR/git/gitconfig" ~/.gitconfig
 
