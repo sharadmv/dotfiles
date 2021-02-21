3b812685fc565ca507e779e15ac72f821de8e758
Sharad Vikram <sharad.vikram@gmail.com>
2021-02-20 18:28:51 -0800

    latest dotfiles

--- install.sh
+++ install.sh
@@ -6,13 +6,15 @@ EXTRA_DIR="$HOME/.extra"
 
 [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
-curl http://j.mp/spf13-vim3 -L -o - | sh
-
 ln -sfv "$DOTFILES_DIR/bash/bashrc" ~/.bashrc.local
 ln -sfv "$DOTFILES_DIR/bash/bash_aliases" ~/.bash_aliases
 ln -sfv "$DOTFILES_DIR/bash/bash_functions" ~/.bash_functions
 ln -sfv "$DOTFILES_DIR/bash/bash_git" ~/.bash_git
 ln -sfv "$DOTFILES_DIR/bash/bash_go" ~/.bash_go
+ln -sfv "$DOTFILES_DIR/bash/bash_aws" ~/.bash_aws
+
+ln -sfv "$DOTFILES_DIR/fish/config.fish" ~/.config/fish/config.fish
+ln -sfv "$DOTFILES_DIR/fish/shell.fish" ~/.config/fish/shell.fish
 
 ln -sfv "$DOTFILES_DIR/vim/vimrc.local" ~/.vimrc.local
 ln -sfv "$DOTFILES_DIR/vim/vimrc.bundles.local" ~/.vimrc.bundles.local
